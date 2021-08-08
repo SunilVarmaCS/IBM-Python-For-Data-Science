ee325ab9e92da2ab502ae3a33125295b63eb68b6
Sunil Varma <74674619+SunilVarmaCS@users.noreply.github.com>
2021-08-08 14:28:16 +0530

    Add files via upload

---
+++ COGNATIVE_2_.py
@@ -0,0 +1,140 @@
+
+#Conditions and Branching
+
+#Loops 
+
+#Functions
+
+#Objects and Classes
+
+a = 6
+print(a==6)
+
+print(6==6)
+print(7==7)
+
+#Another Example 
+
+i = 6
+
+print(i > 5)
+print(i >= 5)
+#i  = 2
+print(i < 2)
+print(i <= 2)
+
+#Inequality Test uses a Explanation Mark
+
+i!=6
+
+print(i!=6)
+
+#Equality Test (==) and Inequality Test (!=)
+
+print("Same" == "Creativity")
+print("Same" != "Creativity")
+
+#Branchings
+
+#Branching allows us to run different statements for different input.
+
+#The "if" Statement
+age = 19
+if (age > 18):
+    print("You can Enter the concert")
+print("You can Move on")
+
+#The "else" Statement 
+age = 17
+if (age > 18):
+    print("You can Enter the Concert")
+else:
+    print("Meet to the Staff Members")
+
+print("Then You can Go the Concert")
+
+#The Elif Statement
+
+age = 20
+
+if(age > 18):
+    print("You can Enter")
+
+elif(age == 18):
+    print("Go to the Pink Floyd")
+
+else:
+    print("Go to Staff Office")
+
+print("Then you can move on")
+
+#Logic Operators
+
+#Logic Operations take boolean values and produce different boolean values  
+
+#Not Operator
+
+print(not(True))
+print(not(False))
+
+#or Operator
+
+A = False
+B = False
+print(A or B)
+
+A = False
+B = True
+print(A or B)
+
+A = True
+B = False
+print(A or B)
+
+A = True
+B = True
+print(A or B)
+
+album_year = 1990
+
+if (album_year < 1980) or (album_year > 1989):
+    print("The Album was made in the 70's or 90's")
+else:
+    print("The album was made in the 1980's")
+
+#And Operations
+
+A = False
+B = False
+print(A and B)
+print(A & B)
+
+album_year = 1983
+
+if (album_year > 1979) and (album_year < 1990):
+    print("This album was made in the 80's")
+
+#Branching allows to different statements to different inputs
+
+#Loops
+
+#For Loops and While Loops
+
+#range() Function
+i = [10,20,(30)]
+
+
+name = 20
+
+print(type(name))
+
+
+
+
+
+
+
+
+
+
+
